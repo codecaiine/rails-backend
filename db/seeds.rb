6513e4c210794836238b1a82117a9982e86c36c5
codecaiine <akabrouyannickn@gmail.com>
2022-02-08 23:13:35 +0000

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+greeting_one = Message.create(greeting: "Bonjour, Tout le monde!")
+greeting_two = Message.create(greeting: "Hello Houston")
+greeting_three = Message.create(greeting: "Bonsoir, Monsieur!")
+greeting_four = Message.create(greeting: "Bonsoir, Madame")
+greeting_five = Message.create(greeting: "Hello World, I am groot ")
